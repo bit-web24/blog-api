e7f1d943e35e594e6d49d232d9c0caa3c3e28d0d
bit-web24 <bitweb24@gmail.com>
2023-05-23 16:11:31 +0530

    change routes and added testing libraries

--- routes/routes.js
+++ routes/routes.js
@@ -7,14 +7,14 @@ const authController = require('../controllers/authController');
 const postController = require('../controllers/postController');
 
 // Authentication routes
-router.post('/auth/register', authController.register);
-router.post('/auth/login', authController.login);
+router.post('/api/auth/register', authController.register);
+router.post('/api/auth/login', authController.login);
 
 // Blog post routes
-router.get('/posts', authenticateToken, postController.getAllPosts);
-router.get('/posts/:id', authenticateToken, postController.getPostById);
-router.post('/posts', authenticateToken, postController.createPost);
-router.put('/posts/:id', authenticateToken, postController.updatePost);
-router.delete('/posts/:id', authenticateToken, postController.deletePost);
+router.get('/api/posts', authenticateToken, postController.getAllPosts);
+router.get('/api/posts/:id', authenticateToken, postController.getPostById);
+router.post('/api/posts', authenticateToken, postController.createPost);
+router.put('/api/posts/:id', authenticateToken, postController.updatePost);
+router.delete('/api/posts/:id', authenticateToken, postController.deletePost);
 
 module.exports = router;
